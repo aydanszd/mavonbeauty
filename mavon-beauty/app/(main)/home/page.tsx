16fc27e648acf9010cef46f03984353d7c83d2d8
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-01-22 03:20:14 +0400

    3-column product grid completed

--- mavon-beauty/app/(main)/home/page.tsx
+++ mavon-beauty/app/(main)/home/page.tsx
@@ -1,7 +1,11 @@
 import HeroBanner from "@/Sections/Home/HerroBanner";
+import ProductCards from "@/Sections/Home/ProductCards";
 
 export default function HomePage() {
     return (
-        <HeroBanner/>
+        <>
+            <HeroBanner/>
+            <ProductCards/>
+        </>
     )
 }
