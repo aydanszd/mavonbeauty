09a0232ecb13962e2fa8c1b3180ff0b4541d5c95
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-01-22 22:10:33 +0400

    Update subscription email section on homepage

--- mavon-beauty/Sections/Home/SubsEmail.tsx
+++ mavon-beauty/Sections/Home/SubsEmail.tsx
@@ -1,8 +1,8 @@
 export default function SubsEmail() {
   return (
-    <div className="bg-[#fcf5ee] max-w-7xl mx-auto  py-10 px-14 mt-20 ">
+    <div className="bg-[#fffbf3] max-w-7xl mx-auto  py-10 px-14 mt-20 ">
       <div className="mx-auto px-10">
-        <div className="bg-[#fdf4eb] grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-12 py-4">
+        <div className="bg-[#fffbf3] grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-12 py-4">
 
           <div className="w-full flex flex-col justify-center gap-4">
             <h1 className="text-4xl text-[#333] font-bold tracking-wide">
