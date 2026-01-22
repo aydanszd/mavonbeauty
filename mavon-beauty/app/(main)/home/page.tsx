f36a98078fa814032ff96b2f71bfb66fc3402cc8
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-01-22 04:32:26 +0400

    Featured products section completed

--- mavon-beauty/app/(main)/home/page.tsx
+++ mavon-beauty/app/(main)/home/page.tsx
@@ -1,6 +1,8 @@
+import FeaturedProducts from "@/Sections/Home/FeaturedProducts";
 import HeroBanner from "@/Sections/Home/HerroBanner";
 import MostPopular from "@/Sections/Home/MostPopular";
 import ProductCards from "@/Sections/Home/ProductCards";
+import ShopByCollection from "@/Sections/Home/Shopbycollection";
 
 export default function HomePage() {
     return (
@@ -8,6 +10,8 @@ export default function HomePage() {
             <HeroBanner/>
             <ProductCards/>
             <MostPopular/>
+            <ShopByCollection/>
+            <FeaturedProducts/>
         </>
     )
 }
